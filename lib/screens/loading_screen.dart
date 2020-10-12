84dffabbe19bc93620eb85d30010799ce0f1b0f0
vijay gohel <gohil5898@gmail.com>
2020-10-12 16:22:53 +0530

    understood API calls

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,14 +13,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
 
-    void getLocation() async {
-      Location location = Location();
-      await location.getCurrentLocation();
-      print(location.latitude);
-      print(location.longitude);
-    }
-
     getLocation();
+    getResponse();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getResponse() async {
+    http.Response response = await http.get('http://google.com/');
+    if (response.statusCode == 200)
+      print(response.body);
+    else
+      print(response.statusCode);
   }
 
   @override
