f7de9d62c96b7d2b0cbedbb59c5e39b7469d64a3
vijay gohel <gohil5898@gmail.com>
2020-10-12 14:38:18 +0530

    refactored location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,19 +8,17 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    try {
-      Position position =
-          await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
-  }
-
   @override
   void initState() {
     super.initState();
+
+    void getLocation() async {
+      Location location = Location();
+      await location.getCurrentLocation();
+      print(location.latitude);
+      print(location.longitude);
+    }
+
     getLocation();
   }
 
